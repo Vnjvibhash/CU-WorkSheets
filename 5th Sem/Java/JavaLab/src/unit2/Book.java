eee2ce289a4fd21d975f8c962f6521826da31b2e
Vivek <vnjvibhash@gmail.com>
2022-10-09 11:46:34 +0530

    ML worksheet 4

---
+++ 5th Sem/Java/JavaLab/src/unit2/Book.java
@@ -0,0 +1,14 @@
+package unit2;
+
+public class Book {
+	private int bookId;
+	private String bookName,bookAuther,bookPublisher;
+	public Book(int bookId, String bookName, String bookAuther, String bookPublisher) {
+		this.bookId = bookId;
+		this.bookName = bookName;
+		this.bookAuther = bookAuther;
+		this.bookPublisher = bookPublisher;
+	}
+	
+	
+}
